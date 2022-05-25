eadcc76205aed0c8eba7f621675652f9f6a71753
dinaneka <76057798+dinaneka@users.noreply.github.com>
2022-05-25 10:10:04 +0800

    add normal user
    add normal user for authentication

--- src/auth.js
+++ src/auth.js
@@ -1,3 +1,4 @@
+
 // import 3rd party modules
 const jwt = require('jsonwebtoken');
 
@@ -26,6 +27,23 @@ const authenticateMobile = (username, password) => {
   throw new Error("CREDENTIAL ERROR::unvalid username or password");
 }
 
+const authenticateUser = (username, password) => {
+  // USER_NORMAL in .env
+  // PASSWORD_USER in .env
+  // SECRET_KEY in .env 
+  if (username === process.env.NORMAL_USER && password === process.env.PASSWORD_USER) {
+    // sign jwt
+    // jwt docs https://jwt.io/introduction
+    const token = jwt.sign({
+      expiresIn: "1 days",
+      audience: "user",
+      issuer: "backend"
+    }, process.env.SECRET_KEY);
+
+    return token;
+  }
+}
+
 module.exports = {
-  authenticateMobile
+  authenticateMobile, authenticateUser
 }
